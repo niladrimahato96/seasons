84601cf03171d4414564a302bf77cf0cd759795e
Niladri Mahato <nmahato@aapnainfotech.com>
2019-05-21 14:29:24 +0530

    Updates

---
+++ precache-manifest.37ec534ede9dcd6e4e00af6ec7d0e87e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b162515c83c554475e0d",
+    "url": "/seasons/static/js/runtime~main.b162515c.js"
+  },
+  {
+    "revision": "9d7e46cfe80485885923",
+    "url": "/seasons/static/js/main.9d7e46cf.chunk.js"
+  },
+  {
+    "revision": "935dcdbb525fbb911e5c",
+    "url": "/seasons/static/js/1.935dcdbb.chunk.js"
+  },
+  {
+    "revision": "9d7e46cfe80485885923",
+    "url": "/seasons/static/css/main.b18df33c.chunk.css"
+  },
+  {
+    "revision": "c5193f89e1086c059f3efecd31586223",
+    "url": "/seasons/index.html"
+  }
+];
